28f3cc9410ea8d27bc40c3d27f9feb70f03daed8
milind-zendesk <milind.shinde@zendesk.com>
2023-07-24 14:42:18 +0530

    initial commit - Milind

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
 package main
 
-import (
-	"fmt"
-)
+import "github.com/milind-zendesk/toy-robot-go/cmd"
 
 func main() {
-	fmt.Printf("Hello World!")
+	cmd.Execute()
 }
